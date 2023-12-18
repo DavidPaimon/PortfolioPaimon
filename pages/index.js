0061a98dd3b8ee8e881bcfc094c7d568c6223e3d
David Paimon <davidfelipe.fj@gmail.com>
2023-12-18 11:17:34 -0500

    Botón de proyectos

--- pages/index.js
+++ pages/index.js
@@ -19,24 +19,48 @@ const Home = () => {
     <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
       <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container ms-auto">
         {/* title */}
-        <h1 className="h1">
-          Transforming Ideas <br /> Into{' '}
+        <motion.h1 
+         variants={fadeIn('down', 0.3)}
+         initial='hidden'
+         animate='show'
+         exit='hidden'
+         className="h1"
+         >
+          Turning Concepts <br /> Into{' '}
           <span className="text-accent">Digital Reality</span>
-        </h1>
+        </motion.h1>
         {/* subtitle */}
-        <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
-          lorem
-          uyt
-          htgt
-        </p>
+        <motion.p
+         variants={fadeIn('down', 0.3)}
+         initial='hidden'
+         animate='show'
+         exit='hidden'
+         className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+         >
+          David
+          Felipe
+          Florido Jiménez
+        </motion.p>
         {/* btn*/}
         <div className="flex justify-center xl:hidden relative">
           <ProjectsBtn />
         </div>
+        {/* <motion.div
+          variants={fadeIn('down', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className="'hidden xl:flex"
+        >
+          <ProjectsBtn />
+        </motion.div> */}
       </div>
     </div>
     {/* image */}
-    <div>image</div>
+    <div>
+      {/* bg img */}
+      <div className="bg-none"></div>
+    </div>
   </div>
   );
 };
